6a9933c6113c1229180163d95b4196effe761190
igroman4ik-pc <ni4000009@gmail.com>
2026-01-11 16:56:03 +0300

    add vars in styles add lib postcss-pxtorem update libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,16 @@ import playformCompress from '@playform/compress';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [playformCompress()],
+	compressHTML: false,
+	scopedStyleStrategy: 'class',
+
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: "@use '@/styles/helper' as *;",
+				},
+			},
+		},
+	},
 });
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import postcssPresetEnv from 'postcss-preset-env';
+import pxtorem from 'postcss-pxtorem';
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
 	plugins: [],
 };
 
-if (process.argv.includes('build')) {
-	config.plugins = [postcssPresetEnv({ stage: 1 })];
+const isDev = process.env.NODE_ENV === 'development';
+
+if (!isDev) {
+	config.plugins = [pxtorem(), postcssPresetEnv({ stage: 1 })];
 }
 
 export default config;
