14a8380ef375c20ee3f6567c425b0d5c02cb552c
igroman4ik-pc <ni4000009@gmail.com>
2026-06-13 01:15:56 +0300

    feat: add deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yar-acrobatics.ru',
 	integrations: [playformCompress(), sitemap()],
 	compressHTML: false,
 	scopedStyleStrategy: 'class',
